0a6adf4f60ae40bc6a302624375d816098da888d
JKSH <szehowe.koh@gmail.com>
2014-12-11 23:44:40 +0800

    CHANGE: Point to http://doc.qt.io for Qt 5.4
    - The Qt Company has changed hosts
    - Qt 5.3 isn't archived

--- src/background.js
+++ src/background.js
@@ -8,14 +8,21 @@ chrome.omnibox.onInputEntered.addListener(
 
 		var site;
 		switch (qtVersion) {
-			// Docs for older versions of Qt 5 are archived at doc.qt.digia.com
+			// Docs for Qt 5.4 onwards are hosted at doc.qt.io
+			case '5':
+				site = 'site%3Adoc.qt.io%2Fdoc%2Fqt-5%2F'
+				break
+
+			// Docs for Qt 5.3 aren't archived, apparently
+
+			// Docs for Qt 5.0 - 5.2 are hosted at doc.qt.digia.com
 			case '5.2':
 			case '5.1':
 			case '5.0':
 				site = 'site%3Adoc.qt.digia.com%2Fqt-' + qtVersion + '%2F'
 				break
 
-			// Docs for Qt 4.7, 4.8, and the latest Qt 5 are hosted at qt-project.org
+			// Docs for Qt 4.7 and 4.8 are hosted at qt-project.org
 			default:
 				site = 'site%3Aqt-project.org%2Fdoc%2Fqt-' + qtVersion + '%2F'
 		}
