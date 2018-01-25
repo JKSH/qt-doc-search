677b462b91d4d72097de0e3bc35c0f06342a9ae4
JKSH <szehowe.koh@gmail.com>
2018-01-25 21:28:38 +0800

    CHANGE: Refactor

--- src/popup.js
+++ src/popup.js
@@ -1,31 +1,28 @@
+function loadLocalStorage(key, defaultValue) {
+	var value = localStorage[key]
+	if (value == undefined)
+		return defaultValue
+	return value
+}
+
+function initComboBox(elementId, value) {
+	var combobox = document.getElementById(elementId)
+	for (var i = 0; i < combobox.children.length; i++) {
+		var child = combobox.children[i]
+		if (child.value == value) {
+			child.selected = 'true'
+			return
+		}
+	}
+}
+
 // Designed to be called when the popup window is opened
 function loadOptions() {
-	var docSubdir = localStorage['docSubdir']
-	if (docSubdir == undefined) {
-		docSubdir = 'qt-5' // Default to the latest version of Qt 5
-	}
-	var searchEngineBase = localStorage['searchEngineBase']
-	if (searchEngineBase == undefined) {
-		searchEngineBase = 'duckduckgo.com/?q=' // Default to the privacy-centric DuckDuckGo
-	}
-
-	// Highlight the last selections (or the defaults)
-	var combobox = document.getElementById('qtSelection')
-	for (var i = 0; i < combobox.children.length; i++) {
-		var child = combobox.children[i]
-		if (child.value == docSubdir) {
-			child.selected = 'true'
-			break
-		}
-	}
-	combobox = document.getElementById('engineSelection')
-	for (var i = 0; i < combobox.children.length; i++) {
-		var child = combobox.children[i]
-		if (child.value == searchEngineBase) {
-			child.selected = 'true'
-			break
-		}
-	}
+	var docSubdir = loadLocalStorage('docSubdir', 'qt-5') // Default to the latest version of Qt 5
+	var searchEngineBase = loadLocalStorage('searchEngineBase', 'duckduckgo.com/?q=') // Default to the privacy-centric DuckDuckGo
+	
+	initComboBox('qtSelection', docSubdir)
+	initComboBox('engineSelection', searchEngineBase)
 }
 
 // Designed to be called whenever the user changes the comboboxes
