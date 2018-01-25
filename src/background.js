478a4776d90b36cbf054e52c9e9bdae52a848339
JKSH <szehowe.koh@gmail.com>
2018-01-25 20:28:18 +0800

    ADD: Support other search engines
    I picked some of the world's top engines (according to Alexa) favoured
    by different regions. Baidu was in the shortlist, but it didn't support
    the "site:" prefix properly.

--- src/background.js
+++ src/background.js
@@ -4,7 +4,10 @@ chrome.omnibox.onInputEntered.addListener(
 		if (docSubdir == undefined) {
 			docSubdir = 'qt-5' // Default to the latest version of Qt 5
 		}
-
+		var searchEngineBase = localStorage['searchEngineBase']
+		if (searchEngineBase == undefined) {
+			searchEngineBase = 'duckduckgo.com/?q=' // Default to the privacy-centric DuckDuckGo
+		}
 		var site = 'site:doc.qt.io/' + docSubdir + '/'
-		chrome.tabs.create({'url': 'https://www.google.com/search?q=' + site + '+' + encodeURIComponent(searchText)})
+		chrome.tabs.create({'url': 'https://' + searchEngineBase + site + '+' + encodeURIComponent(searchText)})
 	});
--- src/popup.js
+++ src/popup.js
@@ -4,8 +4,12 @@ function loadOptions() {
 	if (docSubdir == undefined) {
 		docSubdir = 'qt-5' // Default to the latest version of Qt 5
 	}
+	var searchEngineBase = localStorage['searchEngineBase']
+	if (searchEngineBase == undefined) {
+		searchEngineBase = 'duckduckgo.com/?q=' // Default to the privacy-centric DuckDuckGo
+	}
 
-	// Highlight the last selected version (or the default)
+	// Highlight the last selections (or the defaults)
 	var combobox = document.getElementById('qtSelection')
 	for (var i = 0; i < combobox.children.length; i++) {
 		var child = combobox.children[i]
@@ -14,12 +18,23 @@ function loadOptions() {
 			break
 		}
 	}
+	combobox = document.getElementById('engineSelection')
+	for (var i = 0; i < combobox.children.length; i++) {
+		var child = combobox.children[i]
+		if (child.value == searchEngineBase) {
+			child.selected = 'true'
+			break
+		}
+	}
 }
 
-// Designed to be called whenever the user changes the combobox
+// Designed to be called whenever the user changes the comboboxes
 function saveOptions() {
 	var combobox = document.getElementById('qtSelection')
 	localStorage['docSubdir'] = combobox.children[combobox.selectedIndex].value
+	
+	combobox = document.getElementById('engineSelection')
+	localStorage['searchEngineBase'] = combobox.children[combobox.selectedIndex].value
 }
 
 // Chrome extensions disallow inline event handlers, so we listen for the
@@ -27,4 +42,5 @@ function saveOptions() {
 document.addEventListener('DOMContentLoaded', function() {
 	loadOptions()
 	document.getElementById('qtSelection').addEventListener('change', saveOptions)
+	document.getElementById('engineSelection').addEventListener('change', saveOptions)
 });
