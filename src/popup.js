7906cdaf50016717cb3520e73ed2ad945f29c0fc
JKSH <szehowe.koh@gmail.com>
2025-03-10 10:54:46 +0800

    popup.js: Store interactive GUI elements in variables
    This helps to simplify other thingss later

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,4 @@
-function initComboBox(elementId, value) {
-	var combobox = document.getElementById(elementId)
+function initComboBox(combobox, value) {
 	for (var i = 0; i < combobox.options.length; i++) {
 		var child = combobox.options[i]
 		if (child.value == value) {
@@ -14,12 +13,16 @@ function loadOptions() {
 	const docSubdir = localStorage['docSubdir'] ?? 'qt-5' // Default to the latest version of Qt 5
 	const searchEngineBase = localStorage['searchEngineBase'] ?? 'duckduckgo.com/?q=' // Default to the privacy-centric DuckDuckGo
 	const openInNewTab = (localStorage['openInNewTab'] ?? 'false') == 'true'
-	
-	initComboBox('qtSelection', docSubdir)
-	initComboBox('engineSelection', searchEngineBase)
-	
+
+	const comboBox_qtSelection = document.getElementById('qtSelection')
+	const comboBox_engineSelection = document.getElementById('engineSelection')
+	const checkBox_tabSwitch = document.getElementById('tabSwitch')
+
+	initComboBox(comboBox_qtSelection, docSubdir)
+	initComboBox(comboBox_engineSelection, searchEngineBase)
+
 	// Init checkbox
-	document.getElementById('tabSwitch').checked = openInNewTab
+	checkBox_tabSwitch.checked = openInNewTab
 }
 
 // Designed to be called whenever the user changes the comboboxes
