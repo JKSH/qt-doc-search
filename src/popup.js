7e74844a75d38ff64f1b98a73df678a6b96779a0
JKSH <szehowe.koh@gmail.com>
2025-03-10 11:01:38 +0800

    popup.js: Merge "change" event listeners into initOptions() function
    - No need for a separate saveOptions() function
    - Only the changed field gets written now

--- src/popup.js
+++ src/popup.js
@@ -9,7 +9,7 @@ function initComboBox(combobox, value) {
 }
 
 // Designed to be called when the popup window is opened
-function loadOptions() {
+function initOptions() {
 	const docSubdir = localStorage['docSubdir'] ?? 'qt-5' // Default to the latest version of Qt 5
 	const searchEngineBase = localStorage['searchEngineBase'] ?? 'duckduckgo.com/?q=' // Default to the privacy-centric DuckDuckGo
 	const openInNewTab = (localStorage['openInNewTab'] ?? 'false') == 'true'
@@ -23,28 +23,22 @@ function loadOptions() {
 
 	// Init checkbox
 	checkBox_tabSwitch.checked = openInNewTab
-}
 
-// Designed to be called whenever the user changes the comboboxes
-function saveOptions() {
-	var box = document.getElementById('qtSelection')
-	localStorage['docSubdir'] = box.options[box.selectedIndex].value
-	
-	box = document.getElementById('engineSelection')
-	localStorage['searchEngineBase'] = box.options[box.selectedIndex].value
-	
-	box = document.getElementById('tabSwitch')
-	localStorage['openInNewTab'] = box.checked // NOTE: Implicit stringification occurs
+	// Save user selections whenever they are changed
+	comboBox_qtSelection.addEventListener('change', function() {
+		localStorage['docSubdir'] = comboBox_qtSelection.options[comboBox_qtSelection.selectedIndex].value
+	})
+	comboBox_engineSelection.addEventListener('change', function() {
+		localStorage['searchEngineBase'] = comboBox_engineSelection.options[comboBox_engineSelection.selectedIndex].value
+	})
+	checkBox_tabSwitch.addEventListener('change', function() {
+		localStorage['openInNewTab'] = checkBox_tabSwitch.checked // NOTE: Implicit stringification occurs
+	})
 }
 
 // Chrome extensions disallow inline event handlers, so we listen for the
 // DOMContentLoaded event instead to initialize this tool at startup
-document.addEventListener('DOMContentLoaded', function() {
-	loadOptions()
-	document.getElementById('qtSelection').addEventListener('change', saveOptions)
-	document.getElementById('engineSelection').addEventListener('change', saveOptions)
-	document.getElementById('tabSwitch').addEventListener('change', saveOptions)
-})
+document.addEventListener('DOMContentLoaded', initOptions)
 
 // Allow popup.html to open links in a new tab.
 document.addEventListener('click', function(e) {
