d781ae59a67e73a46301a3ab6554f763b7c19666
JKSH <szehowe.koh@gmail.com>
2025-03-10 10:01:37 +0800

    background.js: Simplify localStorage reads with nullish coalescing

--- src/background.js
+++ src/background.js
@@ -1,17 +1,9 @@
 chrome.omnibox.onInputEntered.addListener(
 	function(searchText) {
 		// NOTE: We can't assume that the popup window has been opened before this point
-		var docSubdir = localStorage['docSubdir']
-		if (docSubdir == undefined)
-			docSubdir = 'qt-5' // Default to the latest version of Qt 5
-
-		var searchEngineBase = localStorage['searchEngineBase']
-		if (searchEngineBase == undefined)
-			searchEngineBase = 'duckduckgo.com/?q=' // Default to the privacy-centric DuckDuckGo
-
-		var openInNewTab = localStorage['openInNewTab']
-		if (openInNewTab == undefined)
-			openInNewTab = 'true' // Default to historical behaviour
+		const docSubdir = localStorage['docSubdir'] ?? 'qt-5' // Default to the latest version of Qt 5
+		const searchEngineBase = localStorage['searchEngineBase'] ?? 'duckduckgo.com/?q=' // Default to the privacy-centric DuckDuckGo
+		const openInNewTab = (localStorage['openInNewTab'] ?? 'false') == 'true' // Default to historical behaviour
 
 		var site = 'site:doc.qt.io/' + docSubdir + '/'
 		var urlObj = {'url': 'https://' + searchEngineBase + site + '+' + encodeURIComponent(searchText)}
