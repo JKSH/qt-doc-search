e5179697aa1c25a10101a46db4bc291c33f19a94
JKSH <szehowe.koh@gmail.com>
2025-03-10 11:51:53 +0800

    popup.js: Improve indentation
    It was left like this in 263510082192f1758b8e1c37daa9b05fac8d52ab for a cleaner diff

--- src/popup.js
+++ src/popup.js
@@ -10,38 +10,38 @@ function initComboBox(combobox, value) {
 
 // Designed to be called when the popup window is opened
 function initOptions() {
-chrome.storage.local.get(['docSubdir', 'searchEngineBase', 'openInNewTab']).then((localStorage) => {
-	const docSubdir = localStorage['docSubdir'] ?? 'qt-5' // Default to the latest version of Qt 5
-	const searchEngineBase = localStorage['searchEngineBase'] ?? 'duckduckgo.com/?q=' // Default to the privacy-centric DuckDuckGo
-	const openInNewTab = (localStorage['openInNewTab'] ?? false)
+	chrome.storage.local.get(['docSubdir', 'searchEngineBase', 'openInNewTab']).then((localStorage) => {
+		const docSubdir = localStorage['docSubdir'] ?? 'qt-5' // Default to the latest version of Qt 5
+		const searchEngineBase = localStorage['searchEngineBase'] ?? 'duckduckgo.com/?q=' // Default to the privacy-centric DuckDuckGo
+		const openInNewTab = (localStorage['openInNewTab'] ?? false)
 
-	const comboBox_qtSelection = document.getElementById('qtSelection')
-	const comboBox_engineSelection = document.getElementById('engineSelection')
-	const checkBox_tabSwitch = document.getElementById('tabSwitch')
+		const comboBox_qtSelection = document.getElementById('qtSelection')
+		const comboBox_engineSelection = document.getElementById('engineSelection')
+		const checkBox_tabSwitch = document.getElementById('tabSwitch')
 
-	initComboBox(comboBox_qtSelection, docSubdir)
-	initComboBox(comboBox_engineSelection, searchEngineBase)
+		initComboBox(comboBox_qtSelection, docSubdir)
+		initComboBox(comboBox_engineSelection, searchEngineBase)
 
-	// Init checkbox
-	checkBox_tabSwitch.checked = openInNewTab
+		// Init checkbox
+		checkBox_tabSwitch.checked = openInNewTab
 
-	// Save user selections whenever they are changed
-	comboBox_qtSelection.addEventListener('change', function() {
-		chrome.storage.local.set({
-			docSubdir: comboBox_qtSelection.options[comboBox_qtSelection.selectedIndex].value
+		// Save user selections whenever they are changed
+		comboBox_qtSelection.addEventListener('change', function() {
+			chrome.storage.local.set({
+				docSubdir: comboBox_qtSelection.options[comboBox_qtSelection.selectedIndex].value
+			})
+		})
+		comboBox_engineSelection.addEventListener('change', function() {
+			chrome.storage.local.set({
+				searchEngineBase: comboBox_engineSelection.options[comboBox_engineSelection.selectedIndex].value
+			})
+		})
+		checkBox_tabSwitch.addEventListener('change', function() {
+			chrome.storage.local.set({
+				openInNewTab: checkBox_tabSwitch.checked
+			})
 		})
 	})
-	comboBox_engineSelection.addEventListener('change', function() {
-		chrome.storage.local.set({
-			searchEngineBase: comboBox_engineSelection.options[comboBox_engineSelection.selectedIndex].value
-		})
-	})
-	checkBox_tabSwitch.addEventListener('change', function() {
-		chrome.storage.local.set({
-			openInNewTab: checkBox_tabSwitch.checked
-		})
-	})
-})
 }
 
 // Chrome extensions disallow inline event handlers, so we listen for the
